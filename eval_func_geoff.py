e70ad9b9a30042d5f96164c601674d47d32054f9
Xaitin <xaitin120@gmail.com>
2021-04-06 00:29:53 -0700

    Quiescence search reapplied with bug fixed. Heuristic function improved

--- eval_func_geoff.py
+++ eval_func_geoff.py
@@ -3,26 +3,36 @@ from constants import FRIENDLY_SQUARE_VALUES, ENEMY_SQUARE_VALUES
 
 class EvaluationFunction:
 
-    def __init__(self, board, friendly_color):
+    def __init__(self, board, friendly_color, previous_board):
         self._board_state = board
         self._friendly_color = friendly_color
+        self._previous_board = previous_board
 
     def evaluate_move(self):
         marbles = self._board_state.split(',')
+        prev_marbles = self._previous_board[1].split(',')
         square_control_value = 0
         friendly_marble_value = 0
         enemy_marble_value = 0
+        prev_friendly_val = 0
+        prev_enemy_val = 0
         for marble in marbles:
             pos = marble[0] + marble[1]
             if marble[2] == self._friendly_color:
                 value_at_square = FRIENDLY_SQUARE_VALUES.get(pos)
                 if value_at_square is not None:
                     square_control_value += value_at_square
-                friendly_marble_value += 10
+                friendly_marble_value += 1000000
             else:
                 value_at_square = ENEMY_SQUARE_VALUES.get(pos)
                 if value_at_square is not None:
                     square_control_value += value_at_square
-                enemy_marble_value += 10
+                enemy_marble_value += 1000000
+        for m in prev_marbles:
+            if m[2] == self._friendly_color:
+                prev_friendly_val += 1000000
+            else:
+                prev_enemy_val += 1000000
+        prev_marble_value = prev_friendly_val - prev_enemy_val
         marble_count_value = friendly_marble_value - enemy_marble_value
-        return square_control_value + marble_count_value
+        return square_control_value + (marble_count_value - prev_marble_value)
--- game_playing_agent.py
+++ game_playing_agent.py
@@ -1,4 +1,3 @@
-
 import math
 import random
 from datetime import datetime
@@ -19,7 +18,10 @@ class GamePlayingAgent:
         self.greatest_move_value = 0
         self.next_opponent_moves_values = None
         self.next_opponents_moves = None
+        self.next_move_marble_difference = list()
 
+    def search_deeper(self):
+        pass
 
     def get_ssg_list_position_2d(self, input_list):
         self._state_space_gen.read_input_list(input_list)
@@ -33,15 +35,13 @@ class GamePlayingAgent:
             for marble in line:
                 stri += marble + ","
             self.next_move_board_states.append(stri[:-1])
+        self.next_move_marble_difference = self.find_marble_difference_in_states(self._input_list[1], self.next_move_board_states)
         self.next_moves = self._state_space_gen.state_space.get_move_list()
         self.next_moves_values = self.assign_move_values()
-        # depth 2
-        self.next_moves, self.next_move_board_states, self.next_moves_values = self.find_best_next_moves()
-        self.next_opponent_moves_values, self.next_opponents_moves = self.find_next_opponent_moves()
-        # depth 4
-        self.next_moves, self.next_move_board_states, self.next_moves_values = self.find_best_next_moves()
-        self.next_opponent_moves_values, self.next_opponents_moves = self.find_next_opponent_moves()
-        # depth 6
+        print(self.next_moves)
+        print(self.next_move_board_states)
+        print(self.next_moves_values)
+        self.search_deeper()
         self.next_moves, self.next_move_board_states, self.next_moves_values = self.find_best_next_moves()
         self.next_opponent_moves_values, self.next_opponents_moves = self.find_next_opponent_moves()
         if len(self.next_moves) == 1:
@@ -64,13 +64,14 @@ class GamePlayingAgent:
             move_to_choose = indexes[random.randint(0, len(indexes) - 1)]
             print(self.next_moves[move_to_choose])
             print(self.next_move_board_states[move_to_choose])
+            print(self.next_moves_values[move_to_choose])
             return self.next_moves[move_to_choose], self.next_move_board_states[move_to_choose]
         # This is our list of best moves and states with a depth of 2
 
     def assign_move_values(self):
         values = list()
         for i in range(len(self.next_moves)):
-            eval_func = ef(self.next_move_board_states[i], self._agent_color)
+            eval_func = ef(self.next_move_board_states[i], self._agent_color, self._input_list)
             value = eval_func.evaluate_move()
             values.append(value)
         return values
@@ -79,6 +80,12 @@ class GamePlayingAgent:
         best_moves = list()
         best_states = list()
         best_values = list()
+        for i in range(len(self.next_moves)):
+            if self.next_move_marble_difference[i] != 0:
+                self.next_moves_values[i] = self.quiescence_search(self.next_move_board_states[i])
+                print("Reassigning value via quiescence search")
+                print(self.next_moves_values[i])
+                print("Break")
         highest_move_value = -math.inf
         for i in range(len(self.next_moves)):
             if self.next_moves_values[i] > highest_move_value:
@@ -89,6 +96,63 @@ class GamePlayingAgent:
                 best_states.append(self.next_move_board_states[z])
                 best_values.append(self.next_moves_values[z])
         return best_moves, best_states, best_values
+
+    def quiescence_search(self, state):
+        if self._agent_color == "b":
+            color = "w"
+            start_color = "b"
+        else:
+            color = "b"
+            start_color = "w"
+        my_ssg = ssg()
+        initial_list = list()
+        initial_list.append(color)
+        initial_list.append(state)
+        found_non_volatile_move = False
+        while found_non_volatile_move is not True:
+            my_ssg.read_input_list(initial_list)
+            next_move_board_states = list()
+            board_states = my_ssg.state_space.generate_all_resulting_board_states()
+            for line in board_states:
+                stri = ""
+                for marble in line:
+                    stri += marble + ","
+                next_move_board_states.append(stri[:-1])
+            next_move_values = self.find_marble_difference_in_states(initial_list[1], next_move_board_states)
+            end_state = True
+            new_states = list()
+            new_values = list()
+            highest_value = -math.inf
+            for i in range(len(next_move_values)):
+                if next_move_values[i] != 0:
+                    end_state = False
+                    eval_func = ef(next_move_board_states[i], color, self._input_list)
+                    value = eval_func.evaluate_move()
+                    if value > highest_value:
+                        highest_value = value
+                    new_states.append(next_move_board_states[i])
+                    new_values.append(value)
+            if end_state:
+                if color == start_color:
+                    # Opponent was last to move. Return highest value. Its highest value of next moves
+                    if highest_value == -math.inf:
+                        eval_func = ef(initial_list[1], start_color, self._input_list)
+                        highest_value = eval_func.evaluate_move()
+                    return highest_value
+                else:
+                    # Agent was last to move. Grab value of initial_list[1]
+                    eval_func = ef(initial_list[1], start_color, self._input_list)
+                    return eval_func.evaluate_move()
+            if color == "w":
+                color = "b"
+            elif color == "b":
+                color = "w"
+            if not end_state:
+                for z in range(len(new_states)):
+                    if new_values[z] == highest_value:
+                        initial_list = list()
+                        initial_list.append(color)
+                        initial_list.append(new_states[z])
 
     def find_next_opponent_moves(self):
         next_opponent_move_values = list()
@@ -112,12 +176,12 @@ class GamePlayingAgent:
                 next_opponent_board_states.append(_str[:-1])
             greatest_opponent_move_value = -math.inf
             for opponent_state in next_opponent_board_states:
-                eval_func = ef(opponent_state, color)
+                eval_func = ef(opponent_state, color, self._input_list)
                 current_move_value = eval_func.evaluate_move()
                 if current_move_value > greatest_opponent_move_value:
                     greatest_opponent_move_value = current_move_value
             for i in range(len(move_list)):
-                eval_func = ef(next_opponent_board_states[i], color)
+                eval_func = ef(next_opponent_board_states[i], color, self._input_list)
                 value = eval_func.evaluate_move()
                 if value == greatest_opponent_move_value:
                     next_opponent_moves.append(move_list[i])
@@ -135,6 +199,16 @@ class GamePlayingAgent:
         self.greatest_move_value = 0
         self.next_opponent_moves_values = None
         self.next_opponents_moves = None
+        self.next_move_marble_difference = list()
+
+    def find_marble_difference_in_states(self, check, next_states):
+        check_list = check
+        return_list = list()
+        for i in range(len(next_states)):
+            counter = len(check_list) - len(next_states[i])
+            return_list.append(counter)
+        return return_list
+
 
 
 def main():
@@ -156,12 +230,13 @@ def main():
         new_move_w, new_state_w = agent.make_turn()
         agent.set_input_list(["b",
                               new_state_w])
-        if running_count == 20:
+        if running_count == 5:
             running = False
     end_time = datetime.now()
     exe_time = end_time - start_time
     print(f"\nend time: {end_time}")
     print(f"execution time (sec): {exe_time.total_seconds()}")
+
 
 if __name__ == '__main__':
     main()
